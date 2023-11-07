fc5b924c78f45398618c8d3809690621cdb28163
geovana162007 <148897462+geovana162007@users.noreply.github.com>
2023-11-07 14:36:39 -0300

    adicionando texto e tegs

---
+++ index.h
@@ -0,0 +1,112 @@
+medicina
+A Medicina é uma área da ciência e da saúd E que se dedica ao diagnóstico, prevenção e tratamento de doenças, bem como à manutenção e
+ recuperação da saúde humana. 
+É uma profissão que requer um extenso conhecimento dos aspectos do corpo humano, desde a anatomia e fisiologia até a patologia e farmacologia.
+ No campo da Medicina, os profissionais são responsáveis por avaliar os sintomas, realizar exames clínicos, solicitar e interpretar exames laboratoriais 
+ e de imagem, além de prescrever medicamentos e procedimentos terapêuticos adequados. Eles trabalham para identificar as causas das condições médicas e 
+ desenvolver um plano de tratamento individualizado. Existem várias especialidades dentro da Medicina, como cardiologia, dermatologia, pediatria,
+  ginecologia, neurologia, entre outras. Cada especialidade tem seu foco em condições médicas específicas e requer treinamento adicional após 
+  a faculdade de medicina.
+
+A prática médica é regida por princípios éticos, com o objetivo principal de cuidar e promover a saúde dos pacientes. Os médicos devem ser compassivos,
+ empáticos e dedicados ao bem-estar dos indivíduos que atendem. A Medicina é uma área em constante evolução, com avanços científicos 
+ e tecnológicos que contribuem para diagnósticos mais precisos e tratamentos mais eficazes. Os profissionais precisam estar atualizados e continuar
+  aprendendo ao longo de suas carreiras para garantir a melhor qualidade de atendimento aos pacientes.Em resumo, a Medicina é uma disciplina complexa
+   que busca cuidar da saúde e tratar doenças, envolvendo diagnóstico, prevenção e tratamento.
+
+   Os médicos desempenham um papel crucial na sociedade, proporcionando cuidados médicos e melhorando a qualidade de vida das pessoas.
+Medicina é uma das muitas áreas do conhecimento ligada à manutenção e restauração da saúde. Ela trabalha, num sentido amplo, 
+com a prevenção e cura das doenças humanas e animais num contexto médico. Lida com ações de saúde pública e ambiental, incluindo a saúde animal, 
+promoção, prevenção, controle, erradicação e tratamento das doenças, traumatismos ou qualquer outro agravo à integridade e bem-estar animais, além do 
+controlo de sanidade dos produtos e subprodutos de origem animal para o consumo humano e animal compreendem a área da medicina da responsabilidade do
+ profissional de saúde médico veterinário.
+
+O que é MEDICINA?
+
+ 
+
+A MEDICINA é a ciência que estuda a saúde como um todo. Seu objetivo é prevenir e combater doenças
+, manter a qualidade de vida e promover o bem-estar, seja ele individual ou coletivo. Difere-se de acordo com aspectos culturais
+ e religiosos de determinados povos e regiões do planeta, mas em sua essência, se preocupa com a cura das doenças.
+
+ 
+
+O trabalho do médico possui duas principais vertentes: a da pesquisa e a da clínica. Em atividades de pesquisa o profissional 
+usa seus conhecimentos a fim de descobrir as causas, os agentes e o tratamento de determinadas doenças. Na vertente clínica o 
+médico está em contato direto com paciente e também investiga os melhores tratamentos para as doenças.
+
+ 
+
+Apesar de os primeiros registros remeterem ao Antigo Egito, o grego Hipócrates (460 – 377 a.C.) foi quem levou o título de 
+“pai da MEDICINA”. Intelectual, buscava compreender o funcionamento do organismo humano e procurava respostas para diagnosticar 
+doenças de seus pacientes. Já durante a Idade Média, a queda do Império Romano espalha no Ocidente o cristianismo, com sua 
+própria concepção sobre a vida e as doenças.
+
+ 
+
+A MEDICINA, então, desenvolveu-se principalmente durante o século XIX, quando cientistas como o francês Louis Pasteur 
+descobrem que muitas enfermidades são causadas por seres vivos minúsculos – os micro-organismos. Já no século XX destaca-se 
+a invenção da penicilina pelo médico britânico Alexander Fleming. Ele não só reduziu a mortalidade de soldados durante a I 
+Guerra Mundial, como também levou o Prêmio Nobel de MEDICINA em 1945 por dar início à era dos antibióticos. Desde então, 
+aliada à tecnologia, a carreira evolui de maneira cada vez mais eficiente e surpreendente.
+
+ 
+
+Especializações
+• Alergologia e Imunologia – Trata alergias e estuda os mecanismos de defesa do organismo contra antígenos, moléculas que ativam os anticorpos.
+• Anestesiologia – dedica-se a anestesias, sendo o principal auxílio dos cirurgiões.
+• Angiologista – estuda o funcionamento dos vasos sanguíneos e linfáticos.
+• Cancerologia ou oncologia – responsável pelo diagnóstico e tratamento de câncer.
+• Cardiologia – estuda o funcionamento e as doenças do coração, bem como do sistema circulatório.
+• Cirurgia plástica e reconstrutora – dedica-se aos procedimentos de reparação estética ou de danos causados por acidentes.
+• Dermatologia – estuda e trata doenças de pele.
+• Endocrinologia – dedica-se ao estudo do funcionamento das glândulas e efeitos dos hormônios no organismo.
+• Fisiologia – estuda o funcionamento físico-químico das células e preocupa-se com todas as funções dos órgãos e tecidos do corpo humano.
+• Geriatria – estuda o processo de envelhecimento e doenças de idosos.
+• Ginecologia – estuda o funcionamento e trata doenças do aparelho reprodutor feminino.
+• Gastroenterologia – estuda o sistema digestório e as doenças que o acometem.
+• Mastologia – estudo do funcionamento e alterações de mamas.
+• Medicina Nuclear – usa técnicas de imagem para diagnóstico, prevenção e tratamento de doenças.
+• Medicina Legal – utiliza conhecimentos médicos e jurídicos para esclarecimento de fatos de interesse da Justiça.
+• Neurologia – estuda e trata doenças do cérebro e do sistema nervoso.
+• Obstetrícia – ocupa-se da gravidez, do parto e da evolução da saúde feminina no pós-parto.
+• Oftalmologia – estuda e trata doenças dos olhos.
+• Ortopedia e Traumatologia – estudam e tratam doenças e traumas dos ossos e aparelho esquelético.
+• Otorrinolaringologia – estuda e trata doenças de ouvido e garganta.
+• Pediatria – estuda e trata doenças da infância.
+• Pneumologia – estuda e trata doenças dos pulmões e sistema respiratório.
+• Psiquiatria – estuda e trata doenças e transtornos psíquicos.
+• Radiologia – dedica-se ao estudo e emprego dos raios X e de outras energias, com fins diagnósticos e terapêuticos
+• Urologia – estuda o sistema urinário e também se dedica ao trato de doenças do aparelho reprodutor masculino.
+
+ 
+
+Áreas de atuação profissional
+• Pesquisa acadêmica e clínica em universidades e instituições de pesquisa.
+• Atendimento clínico a pacientes em hospitais e clínicas particulares.
+• Docência.
+
+ 
+
+Para atuar como médico, o profissional deve ter registro no Conselho Regional de Medicina de seu estado, que é vinculado ao Conselho Federal de Medicina.
+
+ 
+
+Onde estudar?
+• Graduação – O curso de Medicina é oferecido por diversas universidades federais e estaduais do país, além de algumas 
+privadas também possuírem o curso, que dura em média seis anos, além dos anos adicionais da residência.
+
+ 
+
+• Pós-graduação – Também é oferecida em diversas universidades, tais como:
+Universidade de São Paulo
+Universidade Federal de Minas Gerais
+Universidade Federal do Rio de Janeiro
+
+ 
+
+Para conhecer mais:
+• Academia Nacional de Medicina
+• Associação Médica Brasileira
+• Escolas Médicas do Brasil
+• Revista da Associação Médica Brasileira
